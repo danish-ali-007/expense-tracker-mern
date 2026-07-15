efd8eda5c548cec18b0ad433c3f3e300769054d8
Danish Ali <danishunofficial08@gmail.com>
2026-07-15 22:57:45 +0530

    Update frontend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://expense-tracker-mern-r3kt.onrender.com",
+  baseURL: "https://expense-tracker-mern-r3kt.onrender.com/api",
 });
 
 export default API;
